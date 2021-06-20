0afa75e5cb5b8408aaca209e7c38fdf61a320afd
bharaghaveshricg <85169355+bharaghaveshricg@users.noreply.github.com>
2021-06-20 22:23:22 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
-var grid = 50;
 var width = 1366;
 var carGroup1,logGroup1;
 var grassHeight = 100;
 var gameState = "play";
-var carAnimation, logAnimation, playerAnimation;
-var school;
 function preload()
 {
  
